b8f3f6153fc1409bd38fdd06fa4ab43299d24527
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2022-11-13 00:28:05 -0800

    afasf

--- mpi_2dmesh.cpp
+++ mpi_2dmesh.cpp
@@ -473,7 +473,7 @@ sobel_filtered_pixel(float *s, int i, int j , int ncols, int nrows, float *gx, f
 }
 
 void
-do_sobel_filtering(float *in, float *out, int ncols, int nrows, int xoffset, int yoffset)
+do_sobel_filtering(float *in, float *out, int ncols, int nrows, int i, int j)
 {
    float Gx[] = {1.0, 0.0, -1.0, 2.0, 0.0, -2.0, 1.0, 0.0, -1.0};
    float Gy[] = {1.0, 2.0, 1.0, 0.0, 0.0, 0.0, -1.0, -2.0, -1.0};
@@ -481,11 +481,11 @@ do_sobel_filtering(float *in, float *out, int ncols, int nrows, int xoffset, int
    // ADD CODE HERE: insert your code here that iterates over every (i,j) of input,  makes a call
    // to sobel_filtered_pixel, and assigns the resulting value at location (i,j) in the output.
 
-   for(int i=xoffset; i<nrows; i++) {
-      for(int j=yoffset; j<ncols; j++) {
+   // for(int i=0; i<nrows; i++) {
+      // for(int j=0; j<ncols; j++) {
          out[i*ncols + j] = sobel_filtered_pixel(in, i, j, ncols, nrows, Gx, Gy);
-      }
-   }
+      // }
+   // }
 }
 
 
@@ -513,8 +513,8 @@ sobelAllTiles(int myrank, vector < vector < Tile2D > > & tileArray) {
 #endif
          // ADD YOUR CODE HERE
          // to call your sobel filtering code on each tile
-            
-            do_sobel_filtering(t->inputBuffer.data(), t->outputBuffer.data(), t->width, t->height, t->xoffset, t->yoffset);
+            if()
+            do_sobel_filtering(t->inputBuffer.data(), t->outputBuffer.data(), t->width, t->height, row, col);
 
          }
       }
