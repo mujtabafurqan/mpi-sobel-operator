3500420887e7144108734ef8f9172be02a5db021
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2022-11-12 17:46:40 -0800

    afasf

--- mpi_2dmesh.cpp
+++ mpi_2dmesh.cpp
@@ -569,7 +569,7 @@ do_sobel_filtering(float *in, float *out, int i, int j,int ncols, int nrows)
    //    for(int j=0; j<ncols; j++) {
          out[i*ncols + j] = sobel_filtered_pixel(in, i, j, ncols, nrows, Gx, Gy);
       // }
-   }
+   // }
 }
 
 
@@ -642,15 +642,15 @@ scatterAllTiles(int myrank, vector < vector < Tile2D > > & tileArray, float *s, 
                printf("xmax : %d", t->ghost_xmax);
                printf("ymin : %d", t->ghost_ymin);
                printf("ymax : %d", t->ghost_ymax);
-               int xloc = (t->xloc) - 1;
-               if(xloc < ghost_xmin) xloc = 0;
-               int yloc = (t->yloc) - 1;
-               if(yloc < ghost_ymin) yloc = 0;
+               // int xloc = (t->xloc) - 1;
+               // if(xloc < t->ghost_xmin) xloc = 0;
+               // int yloc = (t->yloc) - 1;
+               // if(yloc < t->ghost_ymin) yloc = 0;
 
-               int width = (t->width) + 1;
-               if(width> t->ghost_ymax) width = t->width;
-               int height = (t->height) + 1;
-               if(height > t->ghost_xmax) height = t->height;
+               // int width = (t->width) + 1;
+               // if(width> t->ghost_ymax) width = t->width;
+               // int height = (t->height) + 1;
+               // if(height > t->ghost_xmax) height = t->height;
                
                sendStridedBuffer(s, // ptr to the buffer to send
                      global_width, global_height,  // size of the src buffer
