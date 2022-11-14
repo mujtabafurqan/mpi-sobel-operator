94b5bba56a2d4d80f1cbc82462f2efd1d8940a0e
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2022-11-13 17:25:48 -0800

    asasfa

--- mpi_2dmesh.cpp
+++ mpi_2dmesh.cpp
@@ -483,7 +483,7 @@ do_sobel_filtering(float *in, float *out, int ncols, int nrows)
 
    for(int i=0; i<nrows; i++) {
       for(int j=0; j<ncols; j++) {
-         out[(i-xoffset)*ncols + (j-yoffset)] = sobel_filtered_pixel(in, i, j, ncols, nrows, Gx, Gy);
+         out[(i)*ncols + (j)] = sobel_filtered_pixel(in, i, j, ncols, nrows, Gx, Gy);
       }
    }
 }
