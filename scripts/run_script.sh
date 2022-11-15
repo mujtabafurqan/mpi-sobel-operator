56fc094a67877544c9d6c5d0ac0611f487c6d7a0
Mujtaba Furqan <mujtaba.1996.mf@gmail.com>
2022-11-14 16:23:41 -0800

    asag

--- scripts/run_script.sh
+++ scripts/run_script.sh
@@ -8,11 +8,11 @@ export input="../data/zebra-gray-int8-4x"
 export xsize=7112
 export ysize=5146
 
-# for P in 4 9 16 25 36 49 64 81
-for P in 81
+for P in 4 9 16 25 36 49 64 81
+# for P in 9
    do
-   # for decomp in 1 2 3
-   for decomp in 1
+   for decomp in 1 2 3
+   # for decomp in 3
       do
       echo " srun -n $P $1 -i $input -x $xsize -y $ysize -g $decomp  "
       srun -n $P ./mpi_2dmesh -x $xsize -y $ysize -g $decomp 
